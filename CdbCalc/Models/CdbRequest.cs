c283da3633798514932480850e8cc48c05727756
Bruno Borges <brunotomo@gmail.com>
2025-02-14 18:29:22 -0300

    changing_proj

---
+++ CdbCalc/Models/CdbRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CdbCalc.WebApi.Models
+{
+    public class CdbRequest
+    {
+        public decimal ValorInicial { get; set; }  
+        public int Prazo { get; set; }
+    }
+}
---
+++ CdbCalc/Models/CdbResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace CdbCalc.WebApi.Models
+{
+    public class CdbResponse
+    {
+        public decimal ValorBruto { get; set; }  // Valor final sem impostos
+        public decimal ValorLiquido { get; set; } // Valor final com impostos
+    }
+}
